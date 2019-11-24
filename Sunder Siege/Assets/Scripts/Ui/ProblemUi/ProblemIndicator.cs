0e3d259af796f168eb81ff7e79666508cc159c7f
Larry <47731638+HuberAnton@users.noreply.github.com>
2019-11-25 10:47:45 +1100

    Ui pop.
    Ui now pops. Variables are hidden inside script.

--- Sunder Siege/Assets/Scripts/Ui/ProblemUi/ProblemIndicator.cs
+++ Sunder Siege/Assets/Scripts/Ui/ProblemUi/ProblemIndicator.cs
@@ -9,17 +9,68 @@ public class ProblemIndicator : MonoBehaviour
 
 
     private float m_lastPop;
+    private float m_timeBetweenPulse = 1.0f;
 
-    
+    private float m_lastPulse;
 
+    private float m_max = 0.030f;
+    private float m_steps = 0.001f;
+
+    private float m_startScale;
+
+    private bool m_doPulse = false;
+    private bool m_maxReached = false;
     // Update is called once per frame
+
+
+    private void Start()
+    {
+        m_startScale = this.transform.localScale.x;
+    }
     void Update()
     {
         this.transform.LookAt(Camera.main.transform);
+
+        if(m_doPulse)
+        {
+            DoPulse();
+        }
+        else if(m_lastPulse < Time.time)
+        {
+            m_doPulse = true;
+            DoPulse();
+        }
+
     }
     public void Setup(GameObject a_problemToSpawnOver, Vector3 a_uiPostion)
     {
         this.transform.position = a_problemToSpawnOver.transform.position + a_uiPostion;
+        m_lastPulse = Time.time + m_timeBetweenPulse;
+    }
+
+    private void DoPulse()
+    {
+        if(m_maxReached)
+        {
+            // Scales Vector down.
+            this.transform.localScale = this.transform.localScale - new Vector3(m_steps, m_steps, m_steps);
+            if(this.transform.localScale.x <= m_startScale)
+            {
+                this.transform.localScale = new Vector3(m_startScale, m_startScale, m_startScale);
+                m_maxReached = false;
+                m_doPulse = false;
+                m_lastPulse = Time.time + m_timeBetweenPulse;
+            }
+
+        }
+        else
+        {
+            this.transform.localScale = this.transform.localScale + new Vector3(m_steps, m_steps, m_steps);
+             if(this.transform.localScale.x >= m_max)
+            {
+                m_maxReached = true;
+            }
+        }
     }
 
 }
